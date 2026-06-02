bc4435c7c633786400d9a28e12c63711a551d27e
Logapriya D <logapriyad2005@gmail.com>
2026-06-03 01:09:56 +0530

    Add files via upload

---
+++ Variblesdatatypes.java
@@ -0,0 +1,14 @@
+public class Variblesdatatypes {
+
+    public static void main(String[] args) {
+        int age = 20;
+        double cgpa = 8.5;
+        char grade = 'A';
+        String name = "Logapriya";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(grade);
+    }
+}
