27f6c9d667c127950836397930176be4abf86dcd
Logapriya D <logapriyad2005@gmail.com>
2026-06-03 10:55:21 +0530

    Add files via upload

---
+++ AccessArrayElements.java
@@ -0,0 +1,7 @@
+public class AccessArrayElements {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println(numbers[0]);
+        System.out.println(numbers[2]);
+    }
+}
