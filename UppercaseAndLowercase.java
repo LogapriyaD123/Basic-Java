17a70a849c9f52097741c6e3685bdb5f28f5cded
Logapriya D <logapriyad2005@gmail.com>
2026-06-03 12:15:50 +0530

    Add files via upload

---
+++ UppercaseAndLowercase.java
@@ -0,0 +1,7 @@
+public class UppercaseAndLowercase {
+    public static void main(String[] args) {
+        String name = "Logapriya";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
