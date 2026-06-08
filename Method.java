6dc03a671e2640a0f35d008cb0e0ca431c48df57
Logapriya D <logapriyad2005@gmail.com>
2026-06-08 22:01:01 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,8 @@
+public class Method {
+    static void greet() {
+        System.out.println("Welcome to Java");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
---
+++ MethodWithParameters.java
@@ -0,0 +1,9 @@
+public class MethodWithParameters {
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Logapriya");
+    }
+}
