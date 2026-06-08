b0f3574bb91b0f707fc4b2fd4d6a94dd83d6e7df
Logapriya D <logapriyad2005@gmail.com>
2026-06-08 22:10:11 +0530

    Add files via upload

---
+++ MethodCubeOfaNumber.java
@@ -0,0 +1,8 @@
+public class MethodCubeOfaNumber {
+    static int cube(int n) {
+        return n * n * n;
+    }
+    public static void main(String[] args) {
+        System.out.println(cube(3));
+    }
+}
