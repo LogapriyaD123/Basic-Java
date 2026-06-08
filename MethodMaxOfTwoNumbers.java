fc7e205a52cb54583e63f8a3d99652cffc0c9441
Logapriya D <logapriyad2005@gmail.com>
2026-06-08 22:12:45 +0530

    Add files via upload

---
+++ MethodMaxOfTwoNumbers.java
@@ -0,0 +1,11 @@
+public class MethodMaxOfTwoNumbers {
+    static int max(int a, int b) {
+        if(a > b)
+            return a;
+        else
+            return b;
+    }
+    public static void main(String[] args) {
+        System.out.println(max(10, 20));
+    }
+}
